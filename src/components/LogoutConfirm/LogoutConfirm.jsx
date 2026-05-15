a5d807ae3edb985f3bb4ec17964bfcb5c9ab8f65
arcane <arcanesir.neo@gmail.com>
2026-05-15 10:44:29 +0200

    remove comment

--- src/components/LogoutConfirm/LogoutConfirm.jsx
+++ src/components/LogoutConfirm/LogoutConfirm.jsx
@@ -6,23 +6,24 @@ import { clearTableId } from '../../redux/tables/tablesSlice'
 import { closeModal } from '../../redux/modal/modalSlice'
 
 export const LogoutConfirm = () => {
-    const dispatch = useDispatch()
-    const handleSignOut = () => {
-        dispatch(apiSignOut())
-        dispatch(clearTableId())
-        dispatch(closeModal())
-    }
-    const handleClose = () => {
-        dispatch(closeModal())
-    }
+  const dispatch = useDispatch()
+  const handleSignOut = () => {
+    dispatch(apiSignOut())
+    dispatch(clearTableId())
+    dispatch(closeModal())
+  }
+  const handleClose = () => {
+    dispatch(closeModal())
+  }
   return (
     <Container>
-        <h2>You want to logout?</h2>
-    <BtnContainer>
+      <h2>You want to logout?</h2>
+      <BtnContainer>
         <button onClick={handleSignOut}>YES</button>
-        <button onClick={handleClose}>CANCEL</button>
-    </BtnContainer>
+        <button style={{ background: 'transparent' }} onClick={handleClose}>
+          CANCEL
+        </button>
+      </BtnContainer>
     </Container>
   )
 }
-
--- src/components/LogoutConfirm/LogoutConfirm.styled.jsx
+++ src/components/LogoutConfirm/LogoutConfirm.styled.jsx
@@ -1,27 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
-margin: 0 auto;
-width: 400px;
-height: 200px;
-> h2{
+  margin: 0 auto;
+  width: 400px;
+  height: 200px;
+  > h2 {
     font-size: 2em;
     text-align: center;
-}
+    color: var(--white);
+  }
 `
 export const BtnContainer = styled.div`
-min-width: 300px;
-max-width: 400px;
-min-height: 200px;
-max-height: 300px;
-display: flex;
-justify-content: space-around;
-> button{
+  min-width: 300px;
+  max-width: 400px;
+  min-height: 200px;
+  max-height: 300px;
+  display: flex;
+  justify-content: space-around;
+  > button {
     width: 80px;
     height: 30px;
-    border: none;
+    border: 1px solid var(--pink);
     border-radius: 15px;
-    background-color: var(--violet);
+    background-color: var(--pink);
     color: var(--white);
-}
+    transition: box-shadow 0.3s ease-in-out;
+    &:hover,
+    &:active {
+      box-shadow: 0px 9px 30px -4px var(--pink);
+    }
+  }
 `
