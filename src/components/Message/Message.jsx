e1d1cc65459c2fd9be0a6c038d388cd09994cb29
arcane <arcanesir.neo@gmail.com>
2026-05-15 10:49:49 +0200

    add vacation form

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
-import { Container, Title, Msg } from './Message.styles'
+import { useDispatch, useSelector } from 'react-redux'
+import { Container, Title, InputWrap } from './Message.styles'
+import { apiVacationRequest } from '../../redux/message/operations'
+import { selectAuthUserData } from '../../redux/auth/selectors'
+import { toast } from 'react-toastify'
 
 export const Message = () => {
+  const dispatch = useDispatch()
+  const user = useSelector(selectAuthUserData)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const form = e.currentTarget
+    const formData = new FormData(form)
+    const start = formData.get('start') || ''
+    const end = formData.get('end') || ''
+    if (!start || !end) {
+      toast.error('The form is not filled out')
+      return
+    }
+    if (new Date(end) < new Date(start)) {
+      toast.error('End date cannot be earlier than start date')
+      return
+    }
+    const data = {
+      userName: user?.name,
+      startVacation: start,
+      endVacation: end,
+    }
+    try {
+      await dispatch(apiVacationRequest(data))
+      form.reset()
+      toast.success('Request sent')
+    } catch (error) {
+      console.error(error)
+      toast.error('Error')
+    }
+  }
+
   return (
     <Container>
-        <Title>Messages</Title>
-        <Msg></Msg>
+      <Title>Enter vacation date</Title>
+      <form onSubmit={handleSubmit}>
+        <InputWrap>
+          <label htmlFor="start">Start vacation</label>
+          <input id="start" name="start" type="date" />
+          <label htmlFor="end">End vacation</label>
+          <input id="end" name="end" type="date" />
+        </InputWrap>
+        <button type="submit">Sent</button>
+      </form>
     </Container>
   )
 }
-
--- src/components/Message/Message.styles.jsx
+++ src/components/Message/Message.styles.jsx
@@ -1,13 +1,57 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const Container = styled.div`
-display: flex;
-flex-direction: column;
-width: 600px;
-height: 300px;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 99vh;
+  @media (min-width: 769px) {
+    width: 500px;
+    height: 500px;
+  }
+  > form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    > button {
+      margin-top: 70px;
+      width: 340px;
+      height: 50px;
+      color: var(--white);
+      background-color: var(--pink);
+      border-radius: 30px;
+      font-size: 2em;
+      transition: box-shadow 0.3s ease-in-out;
+      &:hover,
+      &:active {
+        box-shadow: 0px 9px 30px -4px var(--pink);
+      }
+    }
+  }
 `
-export const Title = styled.h2``
-export const Msg = styled.span`
-font-size: 1em;
+export const Title = styled.h2`
+  text-align: center;
+  color: var(--white);
+`
+export const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > label {
+    display: block;
+    color: var(--white);
+    font-size: 2em;
+  }
+  > input {
+    width: 340px;
+    height: 50px;
+    border-radius: 30px;
+    color: var(--pink);
+    padding: 10px 90px;
+  }
 `
