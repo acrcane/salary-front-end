1664f0277e71b415772c39dee1373759c52069f9
arcane <arcanesir.neo@gmail.com>
2026-05-15 10:42:12 +0200

    remove comment

--- src/components/layout/Layout.styled.jsx
+++ src/components/layout/Layout.styled.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 export const Main = styled.main`
-    /* padding: 0 12px; */
     width: 100%;
     min-height: calc(var(--vh, 1vh) * 100 - 80px);
-    background-image: url('${import.meta.env.BASE_URL}images/background/assets-cached.png');
+    background-image: url('${import.meta.env.BASE_URL}images/background/endless-constellation.svg');
     background-position: center;
     background-repeat: no-repeat;
-    background-size: 100% 100%;
+    background-size: cover;
 `
