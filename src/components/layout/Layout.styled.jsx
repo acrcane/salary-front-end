bd67ff2208f4c913a291b444391b78cc4320a213
arcane <arcanesir.neo@gmail.com>
2026-05-24 15:11:51 +0200

    fix: styles

--- src/components/layout/Layout.styled.jsx
+++ src/components/layout/Layout.styled.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-export const Main = styled.main`
+export const Main = styled.div`
     width: 100%;
-    min-height: calc(var(--vh, 1vh) * 100 - 80px);
+    /* min-height: calc(var(--vh, 1vh) * 100 - 80px);
     background-image: url('${import.meta.env.BASE_URL}images/background/endless-constellation.svg');
     background-position: center;
     background-repeat: no-repeat;
-    background-size: cover;
+    background-size: cover; */
 `
